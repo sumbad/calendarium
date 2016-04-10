408e14e636274079f3385d3aa2441e8ba3d9c709
sumbad <sumbad@ya.ru>
2016-04-10 23:31:25 +0800

    0.1.0

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import {Calendarium} from './Calendarium';
+
+ReactDOM.render(React.createElement(Calendarium), document.body);
+
---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+var path = require('path');
+var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const _ = require('lodash');
+const CleanPlugin = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+    node_modules: path.join(__dirname, 'node_modules'),
+    distr: path.join(__dirname, 'distr')
+};
+
+var BASE_CFG = {
+    resolve: {
+        extensions: ['', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-loader',
+                include: PATHS.src
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file?name=[path][name].[ext]',
+            },
+            {
+                test:   /\.less$/,
+                loader: "style-loader!css-loader!postcss-loader!less-loader"
+            },
+        ]
+    },
+    postcss: [autoprefixer({ browsers: ['last 2 versions', 'ie 8', 'ie 9'] })]
+};
+
+
+var FRONTEND_CFG = _.merge(BASE_CFG, {
+    target: 'web',
+    context: PATHS.src,
+    entry: {
+        index: './index.tsx'
+    },
+    output: {
+        path: PATHS.distr,
+        filename: 'index.js',
+        publicPath: '/'
+    },
+    plugins: [
+        // Basic Usage
+        new HtmlWebpackPlugin(),
+    ]
+});
+
+
+var DEV_CFG = {
+    devtool: 'source-map',
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        // display only errors to reduce the amount of output
+        stats: 'errors-only',
+        // parse host and port from env so this is easy
+        // to customize
+        host: process.env.HOST,
+        port: process.env.PORT
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
+
+
+var BUILD_CFG = {
+    plugins: [
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false
+        //     }
+        // }),
+        // Setting DefinePlugin affects React library size!
+        // DefinePlugin replaces content "as is" so we need some extra quotes
+        // for the generated code to make sense
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new CleanPlugin([PATHS.distr])
+    ]
+}
+
+
+if (TARGET === 'start' || !TARGET) {
+    FRONTEND_CFG = _.mergeWith(FRONTEND_CFG, DEV_CFG, customizer)
+}
+
+if (TARGET === 'build') {
+    FRONTEND_CFG = _.mergeWith(FRONTEND_CFG, BUILD_CFG, customizer);
+}
+
+
+// The function to customize assigned values.
+function customizer(objValue, srcValue) {
+    if (_.isArray(objValue)) {
+        return objValue.concat(srcValue);
+    }
+}
+
+
+module.exports = [FRONTEND_CFG];
